28debe5beb807bf9ef812f1f08ba6d000737e011
Mathieu Morlighem <43073461+mmorligh@users.noreply.github.com>
2020-08-29 20:35:25 -0700

    CHG: still some work to do

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <fstream>
+#include <cmath>
 using namespace std;
 
 /*I/O stuff {{{*/
@@ -141,6 +141,23 @@ void  FetchData(FILE* fid,int** pmatrix,int* pM,int* pN,const char* data_name){/
 	if(pM)*pM=M;
 	if(pN)*pN=N;
 }/*}}}*/
+void  FetchData(FILE* fid,double* pdouble,const char* data_name){/*{{{*/
+
+	/*output: */
+	double value;
+	int code;
+
+	/*Set file pointer to beginning of the data: */
+	fid=SetFilePointerToData(fid,&code,NULL,data_name);
+
+	if(code!=3)std::cerr <<"expecting a double for \"" << data_name<<"\"";
+
+	/*We have to read a integer from disk. First read the dimensions of the integer, then the integer: */
+	if(fread(&value,sizeof(double),1,fid)!=1) std::cerr<<"could not read scalar";
+
+	/*Assign output pointers: */
+	*pdouble=value;
+}/*}}}*/
 void  FetchData(FILE* fid,double** pmatrix,int* pM,int* pN,const char* data_name){/*{{{*/
 
 	/*output: */
@@ -226,37 +243,46 @@ void Weights(double** pweights,int* index,double* areas,int nbe,int nbv){/*{{{*/
 	 /*Assign output pointer*/
 	 *pweights = weights;
 }/*}}}*/
-void derive_x_elem(double** pdfdx_e,double* f,int* index,double* alpha,int nbe){/*{{{*/
+void derive_x_elem(double* dfdx_e,double* f,int* index,double* alpha,int nbe){/*{{{*/
 
-	/*Allocate output*/
-	double* dfdx_e = new double[nbe];
-
-	/*Prepare derivative*/
+	/*WARNING!! Assume that dfdx_e has been properly allocated*/
 	for(int i=0;i<nbe;i++){
 		int n1 = index[i*3+0]-1;
 		int n2 = index[i*3+1]-1;
 		int n3 = index[i*3+2]-1;
 		dfdx_e[i]=f[n1]*alpha[i*3+0] + f[n2]*alpha[i*3+1] + f[n2]*alpha[i*3+2];
 	}
-
-	/*Assign output pointer*/
-	*pdfdx_e = dfdx_e;
 }/*}}}*/
-void derive_y_elem(double** pdfdx_e,double* f,int* index,double* beta,int nbe){/*{{{*/
+void derive_y_elem(double* dfdy_e,double* f,int* index,double* beta,int nbe){/*{{{*/
 
-	/*Allocate output*/
-	double* dfdx_e = new double[nbe];
+   /*WARNING!! Assume that dfdx_e has been properly allocated*/
+   for(int i=0;i<nbe;i++){
+      int n1 = index[i*3+0]-1;
+      int n2 = index[i*3+1]-1;
+      int n3 = index[i*3+2]-1;
+      dfdy_e[i]=f[n1]*beta[i*3+0] + f[n2]*beta[i*3+1] + f[n2]*beta[i*3+2];
+   }
+}/*}}}*/
+void elem2node(double* f_v,double* f_e,int* index,double* areas,double* weights,int nbe,int nbv){/*{{{*/
 
-	/*Prepare derivative*/
-	for(int i=0;i<nbe;i++){
-		int n1 = index[i*3+0]-1;
-		int n2 = index[i*3+1]-1;
-		int n3 = index[i*3+2]-1;
-		dfdx_e[i]=f[n1]*beta[i*3+0] + f[n2]*beta[i*3+1] + f[n2]*beta[i*3+2];
-	}
+   /*WARNING!! Assume that f_v has been properly allocated*/
 
-	/*Assign output pointer*/
-	*pdfdx_e = dfdx_e;
+   /*Reinitialize output*/
+   for(int i=0;i<nbv;i++) f_v[i] = 0.;
+
+   /*Add contributions from all elements connected to vertex i*/
+   for(int i=0;i<nbe;i++){
+      int n1 = index[i*3+0]-1;
+      int n2 = index[i*3+1]-1;
+      int n3 = index[i*3+2]-1;
+      f_v[n1] += f_e[i]*areas[i];
+      f_v[n2] += f_e[i]*areas[i];
+      f_v[n3] += f_e[i]*areas[i];
+   }
+
+   /*Divide by sum of areas*/
+   for(int i=0;i<nbv;i++) f_v[i] = f_v[i]/weights[i];
+
 }/*}}}*/
 int main(){/*{{{*/
 
@@ -265,29 +291,55 @@ int main(){/*{{{*/
 	FILE* fid = fopen(inputfile,"rb");
    if(fid==NULL) std::cerr<<"could not open file " << inputfile << " for binary reading or writing";
 
-	/*Get Mesh properties*/
-	int nbe,nbv,M,N;
-	int *index;
-	double* x=NULL;
-	double* y=NULL;
+	/*Get All we need from binary file*/
+	int    nbe,nbv,M,N;
+   double g,rho,yts;
+	int    *index            = NULL;
+	int    *vertexonboundary = NULL;
+	double *x                = NULL;
+	double *y                = NULL;
+	double *H                = NULL;
+	double *surface          = NULL;
+	double *rheology_B       = NULL;
+	double *vx               = NULL;
+	double *vy               = NULL;
 	FetchData(fid,&nbe,"md.mesh.numberofelements");
 	FetchData(fid,&nbv,"md.mesh.numberofvertices");
+   FetchData(fid,&g,"md.constants.g");
+   FetchData(fid,&rho,"md.materials.rho_ice");
+   FetchData(fid,&yts,"md.constants.yts");
 	FetchData(fid,&index,&M,&N,"md.mesh.elements");
+   FetchData(fid,&vertexonboundary,&M,&N,"md.mesh.vertexonboundary");
 	FetchData(fid,&x,&M,&N,"md.mesh.x");
 	FetchData(fid,&y,&M,&N,"md.mesh.y");
-
-	/*Get initial guess*/
-	double* vx = NULL;
-	double* vy = NULL;
+	FetchData(fid,&H,&M,&N,"md.geometry.thickness");
+	FetchData(fid,&surface,&M,&N,"md.geometry.surface");
+   FetchData(fid,&rheology_B,&M,&N,"md.materials.rheology_B");
 	FetchData(fid,&vx,&M,&N,"md.initialization.vx");
 	FetchData(fid,&vy,&M,&N,"md.initialization.vy");
-
-	/*ETC, do the same with rheology_B etc*/
 
 	/*Close input file*/
 	if(fclose(fid)!=0) std::cerr<<"could not close file " << inputfile;
 
-	/*Now prepare nodal functions and areas*/
+	/*Constants*/
+	int    n_glen    = 3;
+	int    damp      = 2;
+	double rele      = 1e-1;
+	double eta_b     = 0.5;
+	double eta_0     = 1.e+14 / 2.;
+	int    niter     = 5e6;
+	int    nout_iter = 1000;
+	double epsi      = 1e-8;
+
+   /*Initial guesses (except vx and vy that we already loaded)*/
+	double* etan = new double[nbe];
+	for(int i=0;i<nbe;i++) etan[i] = 1.e+14;
+	double* dVxdt = new double[nbv];
+	for(int i=0;i<nbv;i++) dVxdt[i] = 0.;
+	double* dVydt = new double[nbv];
+	for(int i=0;i<nbv;i++) dVxdt[i] = 0.;
+
+	/*Manage derivatives once for all*/
 	double* alpha   = NULL;
 	double* beta    = NULL;
 	double* areas   = NULL;
@@ -295,21 +347,126 @@ int main(){/*{{{*/
 	NodalCoeffs(&areas,&alpha,&beta,index,x,y,nbe);
 	Weights(&weights,index,areas,nbe,nbv);
 
-	/*Other Constants needed to solve*/
-	int n_glen = 3;
-	int damp = 2;
-	double rele = 1e-1;
-	double eta_b = 0.5;
-	double eta_0 = 1.e+14 / 2.;
-	double niter = 5e6;
-	int nout_iter = 1000;
-	double epsi = 1e-8;
+   /*MeshSize*/
+   double xmin,xmax,ymin,ymax;
+   double* dx_elem = new double[nbe];
+   double* dy_elem = new double[nbe];
+   for(int i=0;i<nbe;i++){
+      int n1 = index[i*3+0]-1;
+      int n2 = index[i*3+1]-1;
+      int n3 = index[i*3+2]-1;
+      xmin = min(min(x[n1],x[n2]),x[n3]);
+      xmax = max(max(x[n1],x[n2]),x[n3]);
+      ymin = min(min(y[n1],y[n2]),y[n3]);
+      ymax = max(max(y[n1],y[n2]),y[n3]);
+      dx_elem[i] = xmax - xmin;
+      dy_elem[i] = ymax - ymin;
+   }
+   double* resolx = new double[nbv];
+   double* resoly = new double[nbv];
+   elem2node(resolx,dx_elem,index,areas,weights,nbe,nbv);
+   elem2node(resoly,dy_elem,index,areas,weights,nbe,nbv);
+   delete [] dx_elem;
+   delete [] dy_elem;
 
-	//double etan [nbe] = {1e14};
-	//int dvxdt [nbv] = {0};
-	//int dvydt [nbv] = {0};
-	//derive_x_elem(vx);
-	//derive_y_elem(vy);
+   /*Physical properties once for all*/
+   double* dsdx = new double[nbe];
+   double* dsdy = new double[nbe];
+   derive_x_elem(dsdx,surface,index,alpha,nbe);
+   derive_y_elem(dsdy,surface,index,beta,nbe);
+   double* Helem = new double[nbe];
+   for(int i=0;i<nbe;i++){
+      Helem[i] = 1./3. * (H[index[i*3+0]-1] + H[index[i*3+1]-1] + H[index[i*3+2]-1]);
+   }
 
+   /*Compute RHS amd ML once for all*/
+   double *ML  = NULL;
+   double *Fvx = NULL;
+   double *Fvy = NULL;
+   for(int i=0;i<nbv;i++){
+      ML[i]  = 0.;
+      Fvx[i] = 0.;
+      Fvy[i] = 0.;
+   }
+   for(int n=0;n<nbe;n++){
+      /*Lumped mass matrix*/
+      for(int i=0;i<3;i++){
+         for(int j=0;j<3;j++){
+            // \int_E phi_i * phi_i dE = A/6 and % \int_E phi_i * phi_j dE = A/12
+            if(i==j)
+             ML[index[n*3+j]-1] += areas[n]/6;
+            else     
+             ML[index[n*3+j]-1] += areas[n]/12;
+         }
+      }
+         /*RHS, 'F ' in equation 22*/
+      for(int i=0;i<3;i++){
+         for(int j=0;j<3;j++){
+            if(i==j){
+               Fvx[index[n*3+i]-1] += -rho*g*H[index[n*3+j]-1]*dsdx[n]*areas[n]/6.;
+               Fvy[index[n*3+i]-1] += -rho*g*H[index[n*3+j]-1]*dsdy[n]*areas[n]/6.;
+            }
+            else{
+               Fvx[index[n*3+i]-1] += -rho*g*H[index[n*3+j]-1]*dsdx[n]*areas[n]/12.;
+               Fvy[index[n*3+i]-1] += -rho*g*H[index[n*3+j]-1]*dsdy[n]*areas[n]/12.;
+            }
+         }
+      }
+   }
+
+   /*Main loop!*/
+	double* eta_nbv = new double[nbv];
+	double* dtVx    = new double[nbv];
+	double* dtVy    = new double[nbv];
+	double* eps_xx  = new double[nbe];
+	double* eps_yy  = new double[nbe];
+	double* eps_xy  = new double[nbe];
+   for(int iter=0;iter<niter;iter++){
+
+      /*Timesteps - GPU KERNEL 1*/
+      elem2node(eta_nbv,etan,index,areas,weights,nbe,nbv);
+      /*Explicit CFL time step for viscous flow, x and y directions*/
+      for(int i=0;i<nbv;i++){
+         dtVx[i] = rho*pow(resolx[i],2)/(4*H[i]*eta_nbv[i]*(1.+eta_b)*4.1);
+         dtVy[i] = rho*pow(resoly[i],2)/(4*H[i]*eta_nbv[i]*(1.+eta_b)*4.1);
+      }
+
+		/*Strain rates - GPU KERNEL 2*/
+		derive_x_elem(eps_xx,vx,index,alpha,nbe);
+		derive_y_elem(eps_yy,vy,index,beta,nbe);
+
+		/*More later...*/
+   }
+
+   /*Cleanup and return*/
+	delete [] index;
+	delete [] vertexonboundary;
+	delete [] x;
+	delete [] y;
+	delete [] H;
+	delete [] surface;
+	delete [] rheology_B;
+	delete [] vx;
+   delete [] vy;
+   delete [] etan;
+   delete [] dVxdt;
+   delete [] dVydt;
+   delete [] alpha;
+   delete [] beta;
+   delete [] areas;
+   delete [] weights;
+   delete [] resolx;
+   delete [] resoly;
+   delete [] dsdx;
+   delete [] dsdy;
+   delete [] Helem;
+   delete [] ML;
+   delete [] Fvx;
+   delete [] Fvy;
+	delete [] dtVx;
+	delete [] dtVy;
+	delete [] eps_xx;
+	delete [] eps_yy;
+	delete [] eps_xy;
 	return 0;
 }/*}}}*/
