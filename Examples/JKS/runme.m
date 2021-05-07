51ec935654d06a61fb445915b7560e74581a7a20
Anjali Sandip <60862184+AnjaliSandip@users.noreply.github.com>
2021-05-07 09:53:21 -0500

    CHG: Changed to uniform spatial resolution

--- Examples/JKS/runme.m
+++ Examples/JKS/runme.m
@@ -2,29 +2,12 @@ steps=[4];
 
 if any(steps==1)
 	disp('	Step 1: Mesh creation'); 
-	md=triangle(model,'Domain.exp',2000);
-	[lat lon] = xy2ll(md.mesh.x,md.mesh.y,+1,45,70);
-	[x2   y2] = ll2xy(lat,lon,+1,39,71);
-
-	%Get observed velocity field on mesh nodes
-	ncdata = [issmdir() 'examples/Data/Greenland_5km_dev1.2.nc'];
-	if ~exist(ncdata,'file'), 
-		error('File not downloaded in Data Directory');
-	end
-	x1   = ncread(ncdata,'x1');
-	y1   = ncread(ncdata,'y1');
-	velx = ncread(ncdata,'surfvelx');
-	vely = ncread(ncdata,'surfvely');
-	vx   = InterpFromGridToMesh(x1,y1,velx',x2,y2,0);
-	vy   = InterpFromGridToMesh(x1,y1,vely',x2,y2,0);
-	vel  = sqrt(vx.^2+vy.^2);
-
-	%refine mesh using surface velocities as metric
-	md=bamg(md,'hmin',1200,'hmax',15000,'field',vel,'err',5);
-	[md.mesh.lat,md.mesh.long]  = xy2ll(md.mesh.x,md.mesh.y,+1,45,70);
-	
+    resol = 5000;
+	md=triangle(model,'Domain.exp',resol);
+    [md.mesh.lat md.mesh.long] = xy2ll(md.mesh.x,md.mesh.y,+1,45,70);
 	save JksMesh md
 end 
+
 if any(steps==2)
 	disp('	Step 2: Parameterization');
 	md=loadmodel('JksMesh');
@@ -74,5 +57,7 @@ if any(steps==4)  %GPU solver
         % md = refine(md); Add this when increasing spatial resolution
 	addpath('../../src/');
 	%md=gpu(md,1.5,.9);
+	damp = 1.5;
+	relaxation = 0.9%
 	gpu_parallelized  %running it as a script to obtain all the script scalars in the workspace
 end
