68017da5d4d251d07e07895c868cdfabf1514d8e
Anjali Sandip <60862184+AnjaliSandip@users.noreply.github.com>
2022-04-14 16:46:48 -0500

    Update helpers.h

--- Examples/JKS8e4/helpers.h
+++ Examples/JKS8e4/helpers.h
@@ -1,3 +1,20 @@
+/* Title
+
+Copyright (C) 2022 Anjali Sandip, Ludovic Raess and Mathieu Morlighem
+ 
+XXX is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+ 
+XXX is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with FastICE. If not, see <http://www.gnu.org/licenses/>. */
+
 /*CPU Code*/
 /*I/O stuff*/
 FILE* SetFilePointerToData(FILE* fid,int* pcode,int* pvector_type,const char* data_name){
