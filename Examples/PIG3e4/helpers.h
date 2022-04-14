bd9c08ff993e21ae4cc52b9ac3686d433022d8f8
Anjali Sandip <60862184+AnjaliSandip@users.noreply.github.com>
2022-04-14 16:48:03 -0500

    Update helpers.h

--- Examples/PIG3e4/helpers.h
+++ Examples/PIG3e4/helpers.h
@@ -1,5 +1,22 @@
+/* FastICE: cuda_scientific support functions 2D GPU
+
+Copyright (C) 2019  Ludovic Raess, Aleksandar Licul et al.
+ 
+FastICE is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+ 
+FastICE is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with FastICE. If not, see <http://www.gnu.org/licenses/>. */
+
 /*CPU Code*/
-/*I/O stuff*/
+/*I/O*/
 FILE* SetFilePointerToData(FILE* fid,int* pcode,int* pvector_type,const char* data_name){
 
     int found  = 0;
