66a79bd16d7d37f073ead9f042d69b5494ff684c
Anjali Sandip <60862184+AnjaliSandip@users.noreply.github.com>
2022-04-28 17:55:04 -0500

    CHG: Added damp and rela plugins

--- src_rev/runme.sh
+++ src_rev/runme.sh
@@ -5,22 +5,15 @@
 
 
 # compile the code
-#nvcc -arch=sm_70 -O3 ssa_fem_pt.cu
-nvcc -arch=sm_70 -O3 -lineinfo ssa_fem_pt.cu
-
-#Generic case
-# nvcc -arch=sm_70 -O3 -lineinfo ssa_fem_pt.cu -Ddmp=$damp -Drela=$rele 
-# damp = 0.1;
-# rele = 0.2;
-
-# run the code and generate NVTX decorators
-#nsys profile -t nvtx  ./a.out   
+damp=0.1
+rele=0.2
+nvcc -arch=sm_70 -O3 -lineinfo ssa_fem_pt.cu -Ddmp=$damp -Drela=$rele 
 
 #To run the code and generate NSIGHT Systems report
-#nsys profile --stats=true  ./a.out   
+nsys profile --stats=true  ./a.out   
 
 #To run the code and generate NSIGHT Compute report
-ncu --set full -o JKS7e5report ./a.out;
+#ncu --set full -o JKS7e5report ./a.out;
 
 
                
