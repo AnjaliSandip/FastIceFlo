73bf20b4592702f87339a7fa6f848d99181d0682
Anjali Sandip <60862184+AnjaliSandip@users.noreply.github.com>
2022-11-07 15:57:50 -0600

    Update runme.m

--- BinFileGeneration/PIG/runme.m
+++ BinFileGeneration/PIG/runme.m
@@ -1,9 +1,5 @@
-%steps=[1:3 6:7];
 
-
-%steps = [1:5 7];
 steps = [1:5];
-%steps =[1];
 
 if any(steps==1) %Mesh Generation #1
 
@@ -73,11 +69,10 @@ if any(steps==4)  %Rheology B inversion
 	%md.stressbalance.restol=0.01;
 	%md.stressbalance.reltol=0.1;
 	%md.stressbalance.abstol=NaN;
-    md.stressbalance.restol=1000;
+        md.stressbalance.restol=1000;
 	md.stressbalance.reltol=NaN;
 	md.stressbalance.abstol=10;
-    %% Change
-    md.settings.solver_residue_threshold = 1.e-3;
+        md.settings.solver_residue_threshold = 1.e-3;
 
 	% Solve
 	md.cluster=generic('name',oshostname,'np',2);
@@ -108,59 +103,15 @@ if any(steps==5)  %drag inversion
 	md.inversion.max_parameters=200*ones(md.mesh.numberofvertices,1);
 
 	% Solve
-     md=solve(md,'Stressbalance');
+        md=solve(md,'Stressbalance');
 
 	% Update model friction fields accordingly
 	md.friction.coefficient=md.results.StressbalanceSolution.FrictionCoefficient;
 
-	save ./Models/PIG_Control_drag md;
+	save ./Models/PIG3e4 md;
 end
 
 
-if any(steps==6)  %mesh2mesh interpolation
-    md = loadmodel('./Models/PIG_Parameterization');
-        
-   disp('Interpolating results on new mesh');
-
-%Rheology B
-load('Rheology_inversion_3000.mat');
-md.materials.rheology_B = InterpFromMeshToMesh2d(index,x,y,B,md.mesh.x,md.mesh.y);
-
-%Drag
-load('Drag_inversion_3000.mat');
-md.friction.coefficient = InterpFromMeshToMesh2d(index,x,y,drag,md.mesh.x,md.mesh.y); 
-
-save ./Models/PIG_Control_drag md;
-
-end
-
-
-if any(steps==7)  %GPU solver
-%load ./Models/PIG_Parameterization md;
-%load('Rheology_inversion_5000.mat');
-%load('Drag_inversion_5000.mat');
-
-load ./Models/PIG_Control_drag
-%save PIG3e4
-% load PIG3e4
-   % md = refine(md);
-
-   % nbe              = md.mesh.numberofelements;
-   % nbv              = md.mesh.numberofvertices;
-    	
- % save PIG5e5 md -v7.3
- 
-% save PIG3e4
-
-	%addpath('../../src/');
-
-       %addpath('../../src/');
-   %damp = 0.6;
-  %relaxation = 0.6;
-   %gpu_parallelized_NC
-  save PIG;
-   
-end
 
 
 
