bf609e833077aea0cc4f6d6f5ed82c262402f40e
Anjali Sandip <60862184+AnjaliSandip@users.noreply.github.com>
2022-11-07 17:59:10 -0600

    Update runme.sh

--- scripts/MTP Peak/runme.sh
+++ scripts/MTP Peak/runme.sh
@@ -9,6 +9,4 @@ nvcc -arch=sm_70 -O3 -lineinfo memcopy_v3.cu
 
 # run the code
 ./a.out
-#nsys profile --stats=true ./a.out
-#ncu --set full -o report ./a.out
-#ncu --set detailed -k PT2_x -o  PT2_xreport ./a.out
+
