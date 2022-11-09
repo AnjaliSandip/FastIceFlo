fa81fbb9d30acc0b1b861bb712dd971ce81252f7
Anjali Sandip <60862184+AnjaliSandip@users.noreply.github.com>
2022-11-09 14:23:57 -0600

    Update vizme.m

--- visu/vizme.m
+++ visu/vizme.m
@@ -1,11 +1,5 @@
-load JKS8e4_wo
-    figure(1),clf
-        subplot(121)
-        title('Ice Velocity_X, meters/year')
-        patch( 'Faces',index, 'Vertices',[x y],'FaceVertexCData',ans.PTsolution.Vx,'FaceColor','interp','EdgeColor','None');
-        colorbar
-        subplot(122)
-         title('Ice Velocity_Y')
-        patch( 'Faces',index, 'Vertices',[x y],'FaceVertexCData',sqrt(ans.PTsolution.Vx.^2 + ans.PTsolution.Vy.^2)*yts,'FaceColor','interp','EdgeColor','None');
-        colorbar
-        drawnow
+
+load JKS2e4
+md.miscellaneous.name = 'output';
+md=loadresultsfromdisk(md, 'output.outbin')
+plotmodel(md,'data',sqrt(md.results.PTsolution.Vx.^2 + md.results.PTsolution.Vy.^2));
