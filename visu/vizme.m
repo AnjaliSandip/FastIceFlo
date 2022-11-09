c58b2f25fa4c00c2c2876e8527048006d362e4bd
Anjali Sandip <60862184+AnjaliSandip@users.noreply.github.com>
2022-11-09 14:35:29 -0600

    Update vizme.m

--- visu/vizme.m
+++ visu/vizme.m
@@ -1,5 +1,5 @@
 
-load JKS3e5
-md.miscellaneous.name = 'output';
-md=loadresultsfromdisk(md, 'output.outbin')
+load example
+md.miscellaneous.name = 'example';
+md=loadresultsfromdisk(md, 'example.outbin')
 plotmodel(md,'data',sqrt(md.results.PTsolution.Vx.^2 + md.results.PTsolution.Vy.^2));
