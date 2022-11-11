e1667f2b5b12a46d6c65e2773ec3c774876087cc
Anjali Sandip <60862184+AnjaliSandip@users.noreply.github.com>
2022-11-11 13:47:28 -0600

    Update runme.sh

--- src/runme.sh
+++ src/runme.sh
@@ -2,9 +2,13 @@
   
 # -> to make it executable: chmod +x runme.sh or chmod 755 runme.sh
 
+#Optimal PT solver parameter values for the glacier model configuration and the corresponding DoF
+damp=0.99;  
+relaxation=0.03;
+stability=0.9;
+
 # compile the code
-nvcc -arch=sm_70 -O3 -lineinfo   ssa_fem_pt_v2.cu
-#nvcc -O3 -lineinfo   ssa_fem_pt_v2.cu
+nvcc -arch=sm_70 -O3 -lineinfo   ssa_fem_pt.cu   -Ddmp=$damp -Dstab=$stability -Drela=$relaxation 
 
 # run the code
 ./a.out
