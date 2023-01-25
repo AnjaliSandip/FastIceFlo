935c7ac0332cee504534783833e65746f0208ec0
Anjali Sandip <60862184+AnjaliSandip@users.noreply.github.com>
2023-01-25 17:27:41 -0600

    Delete vizme.m

--- visu/vizme.m
+++
@@ -1,6 +0,0 @@
-
-
-load Jks8e4
-md.miscellaneous.name = 'output';
-md=loadresultsfromdisk(md, 'output.outbin')
-plotmodel(md,'data',sqrt(md.results.PTsolution.Vx.^2 + md.results.PTsolution.Vy.^2));
